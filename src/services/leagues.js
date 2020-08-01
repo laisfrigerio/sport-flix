b7b455c92a4f51a720264d6805f55285936777c9
Lais Frigério <laiss@gazetadopovo.com.br>
2020-08-01 03:55:43 -0300

    Show more leagues

--- src/services/leagues.js
+++ src/services/leagues.js
@@ -2,7 +2,7 @@ import axios from './axios';
 
 export const fetch = async () => {
     return await axios.get("all_leagues.php")
-        .then(({data}) => data.leagues.slice(0, 20))
+        .then(({data}) => data.leagues.slice(0, 50))
         .catch(() => []);
 }
 
